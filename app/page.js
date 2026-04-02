f43333bdac1d465d8f25ef3708432b7478fc5204
nickgirling256-hue <nickgirling256@gmail.com>
2026-04-02 19:09:05 +0100

    Update Home component layout and styling

--- app/page.js
+++ app/page.js
@@ -1,13 +1,7 @@
 export default function Home() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>My Bookshelf</h1>
-
-      <img
-        src="https://upload.wikimedia.org/wikipedia/en/c/c3/1984first.jpg"
-        alt="1984 cover"
-        style={{ width: 200, height: "auto", display: "block" }}
-      />
+    <div style={{ padding: 30, fontSize: 40 }}>
+      HELLO TEST 123
     </div>
   );
 }
